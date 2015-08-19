fe751b59b4fab7900d77d4bd2e17306cf98df988
Segun <xtreem88@gmail.com>
2015-08-19 12:20:25 +0100

    Created tables and definitions and ability to add and delete

--- tournament.py
+++ tournament.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
+import bleach
 
 
 def connect():
@@ -13,31 +14,44 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    execute_query(['DELETE FROM matches'])
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    execute_query(['DELETE FROM players'])
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute('SELECT COALESCE(count(*),0) FROM players')
+    rows = c.fetchall()
+    db.close()
+    for row in rows:
+        result = row[0]
+    return result
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    execute_query(['INSERT INTO players\
+        (name) VALUES (\'{0}\')'.format(bleach.clean(name),)])
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
+    The first entry in the list should be the player in first place,
+    or a player
     tied for first place if there is currently a tie.
 
     Returns:
@@ -56,16 +70,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -75,3 +89,19 @@ def swissPairings():
     """
 
 
+def execute_query(query_list):
+    db = connect()
+    c = db.cursor()
+    for query in query_list:
+        c.execute(query)
+    db.commit()
+    db.close()
+
+
+def execute_find(query_string):
+    db = connect()
+    c = db.cursor()
+    c.execute(query_string)
+    rows = c.fetchall()
+    db.close()
+    return rows
--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,17 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- table for players
+CREATE TABLE players (
+    id SERIAL primary key, 
+    name text
+    );
+
+-- table for matches
+CREATE TABLE matches (
+    id SERIAL primary key, 
+    winner SERIAL references players(id), 
+    loser SERIAL references players(id)
+    );
+
 
